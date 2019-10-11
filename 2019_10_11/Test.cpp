4f6e31a2bd836bff916e93a8a5ead6ee690d9988
1358263860@qq.com <1358263860@qq.com>
2019-10-11 23:19:27 +0800

    练习

---
+++ 2019_10_11/Test.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+
+// vector底层结构：动态类型的顺序表
+// vector<char>  string
+#include <vector>
+
+void TestVector1()
+{
+	vector<int> v1;
+	vector<int> v2(10, 5);
+	vector<int> v3(v2);
+
+	int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+	vector<int> v4(array, array + sizeof(array) / sizeof(array[0]));
+
+	vector<int> v5{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+
+	for (size_t i = 0; i < v2.size(); ++i)
+		cout << v2[i] << " ";
+	cout << endl;
+
+	for (auto e : v3)
+		cout << e << " ";
+	cout << endl;
+
+	//vector<int>::iterator it = v4.begin();
+	auto it1 = v4.begin();
+	while (it1 != v4.end())
+	{
+		cout << *it1 << " ";
+		++it1;
+	}
+	cout << endl;
+
+
+	auto it2 = v5.rbegin();
+	while (it2 != v5.rend())
+	{
+		cout << *it2 << " ";
+		++it2;
+	}
+	cout << endl;
+}
+
+void TestVector2()
+{
+	vector<int> v5{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+	cout << v5.size() << endl;
+	cout << v5.capacity() << endl;
+	cout << v5.front() << endl;
+	cout << v5.back() << endl;
+
+	v5.push_back(1);
+	v5.push_back(2);
+	v5.push_back(3);
+	cout << v5.size() << endl;
+	cout << v5.capacity() << endl;
+	cout << v5.front() << endl;
+	cout << v5.back() << endl;
+
+	v5.erase(v5.begin());
+	v5.erase(v5.end() - 1);
+
+	auto pos = find(v5.begin(), v5.end(), 5);
+	if (pos != v5.end())
+		v5.erase(pos);
+
+	v5.clear();
+}
+
+void TestVector3()
+{
+	vector<vector<int>> vv;
+	// 10 * 10---> 6
+	vv.resize(10); // 设置总的行数
+
+	// 给每行设置元素
+	for (size_t i = 0; i < 10; ++i)
+	{
+		vv[i].resize(10, 6);
+	}
+}
+
+int main()
+{
+	//TestVector1();
+	//TestVector2();
+	TestVector3();
+	return 0;
+}
